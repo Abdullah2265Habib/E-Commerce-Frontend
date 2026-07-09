99372fd690722a13c8b368c858141f981c11fec7
Abdullah265Habib <abdullah2006habib@gmail.com>
2026-07-09 15:40:59 +0500

    feat: added global not-found

---
+++ e-commerce-frontend/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you requested does not exist.</p>
+      <p>You can explore following pages </p>
+      <p>This page is temporary not available </p>
+    </div>
+  );
+}
